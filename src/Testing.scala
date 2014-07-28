139373960a1a671a75461052a8a101d2e132055b
volhovm <volhovm@mail.ru>
2014-07-28 04:03:56 +0400

    08.1,2,3

--- src/Testing.scala
+++ src/Testing.scala
@@ -1,6 +1,7 @@
 import chap6._
 import chap5._
 import chap7._
+import chap8._
 
 /**
  * @author volhovm
@@ -9,6 +10,7 @@ import chap7._
 
 object Testing extends App{
   println(new Time(12,30).before(new Time(23,40)))
+
   println(Point(2,3))
   println(CardSuit.values.toList.map(CardSuit.isRed))
   println(RGBCube.values.toList.map((a) => "%06x: %s".format(a.id, a)))
@@ -16,4 +18,11 @@ object Testing extends App{
   random.setSeed(System.currentTimeMillis.toInt)
   println(for (i <- 0 to 10) yield random.nextDouble)
   Ex09
+
+  implicit def nTimes(n: Int) = new {
+    def times(foo: => Unit) = 1 to n foreach {_ => foo}
+  }
+  val acc = new SavingAccount
+  7 times println(acc.deposit(200))
+
 }
--- src/chap7/Ex09.scala
+++ src/chap7/Ex09.scala
@@ -5,6 +5,6 @@ package chap7
  *         Created on 7/27/14
  */
 object Ex09 {
-  print(if (isStrong(Console.in.readLine())) System.getProperties.getProperty("user.name") else "Weak password")
+  println(if (isStrong(Console.in.readLine())) System.getProperties.getProperty("user.name") else "Weak password")
   def isStrong(a: String) : Boolean = a.length > 5 && true // add some extra cases
 }
